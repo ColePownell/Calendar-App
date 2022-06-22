e3e89429bd16c967f4a7970051799f63411c4295
Cole P <pownelcg@mail.uc.edu>
2022-06-22 16:28:38 -0400

    Main activity changes commit

--- app/src/main/java/com/example/calendarapp/MainActivity.kt
+++ app/src/main/java/com/example/calendarapp/MainActivity.kt
@@ -1,61 +1,44 @@
 package com.example.calendarapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.calendarapp.ui.theme.CalendarAppTheme
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CalendarAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.main_fragment)
+        dateTV = findViewById(R.id.idTVDate)
+        calendarView = findViewById(R.id.calendarView)
 
+        calendarView
+            .setOnDateChangeListener(
+                CalendarView.OnDateChangeListener { view, year, month, dayOfMonth ->
+                    // In this Listener we are getting values
+                    // such as year, month and day of month
+                    // on below line we are creating a variable
+                    // in which we are adding all the cariables in it.
+                    val Date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+
+                    // set this date in TextView for Display
+                    dateTV.setText(Date)
+                })
         val username = findViewById<EditText>(R.id.username_et)
         val password = findViewById<EditText>(R.id.password_et)
         val loginButton = findViewById<Button>(R.id.login_btn)
 
         loginButton.setOnClickListener {
 
-           var status = if (username.text.toString().equals("username")
+            var status = if (username.text.toString().equals("username")
                 &&password.text.toString().equals("password")) "Logged In Successfully"
             else "Log In Failed"
 
             Toast.makeText(this,status,Toast.LENGTH_SHORT).show()
 
         }
-
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalendarAppTheme {
-        Greeting("Android")
-    }
-}
