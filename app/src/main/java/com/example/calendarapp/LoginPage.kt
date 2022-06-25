d859e3081d25efc67dc096bdf56804fb4b0bca85
samgehrlich <gehrlism@mail.uc.edu>
2022-06-25 00:01:28 -0400

    finished user registration and user database

--- app/src/main/java/com/example/calendarapp/LoginPage.kt
+++ app/src/main/java/com/example/calendarapp/LoginPage.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import java.lang.reflect.Type
 
 class LoginPage : AppCompatActivity() {
+
+    lateinit var handler:DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //Login Page for Calendar App
@@ -16,31 +17,62 @@ class LoginPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_fragment)
 
+        handler = DatabaseHelper(this)
+
+        showHomeLogin()
+
+        register_button.setOnClickListener{
+            showRegistration()
+        }
+
+        saveUserButton.setOnClickListener{
+
+            handler.insertUserData(newUsername.text.toString(), newPassword.text.toString())
+            showHomeLogin()
+        }
+
+
+
         //Grab ID's from login_fragment.xml
 
         val loginBtn = findViewById<Button>(R.id.loginBtn)
         val username = findViewById<EditText>(R.id.username)
         val password = findViewById<EditText>(R.id.password)
 
+
         //Click Listener for submit button
 
         loginBtn.setOnClickListener {
 
               //Sends User to the Main Activity Page After Login
-
-              val intent = Intent(this, MainActivity::class.java)
-              startActivity(intent)
-
-              //Allows user to enter username and password
-
-              var status = if(username.text.toString().equals("Test")
-                  &&password.text.toString().equals("password")) "Logged In Successful" else "Login Failed"
-
-            Toast.makeText(this,status,Toast.LENGTH_SHORT).show()
+            if (handler.userPresent(username.text.toString(), password.text.toString()))
+                Toast.makeText(this,"Login Success", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this,"Login failed Incorrect Username or Password",Toast.LENGTH_SHORT).show()
 
         }
 
     }
+
+    val registration_layout = findViewById<RelativeLayout>(R.id.registration_layout)
+    val home_login = findViewById<LinearLayout>(R.id.home_login)
+    val register_button = findViewById<Button>(R.id.register_button)
+    val saveUserButton = findViewById<Button>(R.id.save_newUsername)
+    val newUsername = findViewById<EditText>(R.id.new_username)
+    val newPassword = findViewById<EditText>(R.id.new_password)
+
+
+    private fun showRegistration() {
+
+        registration_layout.visibility=View.VISIBLE
+        home_login.visibility=View.GONE
+    }
+
+    private fun showHomeLogin() {
+        home_login.visibility=View.VISIBLE
+        registration_layout.visibility=View.GONE
+    }
+
 
 }
 
