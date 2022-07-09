599404176ed212ab04863bed461ad74009e5fd2a
Cole P <pownelcg@mail.uc.edu>
2022-07-09 09:33:17 -0400

    json reading initial steps

--- app/src/main/java/com/example/calendarapp/MainActivity.kt
+++ app/src/main/java/com/example/calendarapp/MainActivity.kt
@@ -10,8 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_fragment)
+        var indexList: ArrayList<Int> = arrayListOf()
         dateTV = findViewById(R.id.idTVDate)
         calendarView = findViewById(R.id.calendarView)
+        //create string from json file
+        val text = resources.openRawResource(R.raw.event)
+            .bufferedReader().use { it.readText() }
 
         calendarView
             .setOnDateChangeListener(
@@ -25,12 +29,24 @@ class MainActivity : AppCompatActivity() {
 
                     // set this date in TextView for Display
                     dateTV.setText(Date)
+
+
+//inital setup for reading json string
+            indexList.clear()
+            var index = 0
+            while (index >= 0)
+                    {
+                    index = findindex(text, Date,index+1)
+                    indexList.add(index)
+                    println(index)
+                    }
                 })
-
-
-
-
         }
+// finds any json events with the specified date
+    fun findindex(inputString: String, whatToFind: String, startIndex: Int = 0): Int {
+        val matchIndex = inputString.indexOf(whatToFind, startIndex)
+        return matchIndex
+    }
     }
 
 
